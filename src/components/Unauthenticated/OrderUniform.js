c20faab36a42243c7166615964015c7ae2d713f8
RMcLeanDev <rmcleandev@gmail.com>
2022-02-01 15:09:25 -0800

    start the uniform order form

--- src/components/Unauthenticated/OrderUniform.js
+++ src/components/Unauthenticated/OrderUniform.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
+import '../../scss/OrderUniform.scss';
 
 function OrderUniform(){
+
+    function submitOrder(e){
+        e.preventDefault();
+    }
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [vin, setVin] = useState("");
+    const [status, setStatus] = useState("Active")
+    const [rental, setRental] = useState("No")
+    const [error, setError] = useState()
+
     return(
-        <div>
-            <h1>Order Uniform form</h1>
+        <div className="orderContainer">
+            <div className="orderInsideContainer">
+                <h1>Order Uniform Here</h1>
+                <form onSubmit={submitOrder}>
+                    <input 
+                        type="text"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                        placeholder = "Name"
+                    />
+                    <input 
+                        type="email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        placeholder = "Email"
+                    />
+                    <button type="submit" style={{"margin-top": "10px"}}>Confirm</button>
+                    {error}
+                </form>
+            </div>
         </div>
     )
 }
--- src/components/Unauthenticated/UnauthenticatedMain.js
+++ src/components/Unauthenticated/UnauthenticatedMain.js
@@ -5,7 +5,8 @@ function UnauthenticatedMain(){
     return(
         <div>
             <div className="UnauthMain">
-                <h1>Unauthenticated</h1>
+                <h1>Can have news or recent company score here.</h1>
+                <h1>Top Drivers.</h1>
             </div>
         </div>
     )

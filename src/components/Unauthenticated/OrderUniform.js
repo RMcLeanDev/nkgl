75e4b481589771c2deb86600ceabdaeee487a9b4
Ryan <ryan@Admins-MacBook-Pro.local>
2022-02-07 20:22:04 -0800

    adjust uniform order screen add labels

--- src/components/Unauthenticated/OrderUniform.js
+++ src/components/Unauthenticated/OrderUniform.js
@@ -26,13 +26,23 @@ function OrderUniform(){
             <div className="orderInsideContainer">
                 <h1>Order Uniform Here</h1>
                 <form onSubmit={submitOrder}>
+                    <label>First Name</label>
                     <input 
                         type="text"
                         value={name}
                         onChange={e => setName(e.target.value)}
-                        placeholder = "Name"
+                        placeholder = "First Name"
                         required
                     />
+                    <label>Last Name</label>
+                    <input 
+                        type="text"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                        placeholder = "Last Name"
+                        required
+                    />
+                    <label>Work Email</label>
                     <input 
                         type="email"
                         value={email}

b63efc235f0acdb33dd913d5a5a0e29302f79cbc
rmcleandev <rmcleandev@gmail.com>
2022-03-04 11:44:58 -0800

    testing ways to change dates

--- src/components/Unauthenticated/UnauthenticatedMain.js
+++ src/components/Unauthenticated/UnauthenticatedMain.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../../scss/UnauthenticatedMain.scss';
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
 import "../../scss/Calendar.scss"
 import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
 function UnauthenticatedMain(props){
 
     let localizer = momentLocalizer(moment)
+    const [startDate, setStartDate] = useState(new Date());
+    const [newDate, setNewDate] = useState(null);
 
-    console.log(props)
-
-    console.log(new Date("March 3, 2022"))
     const myEventsList = [
         {
             title: "Spaghetti",
@@ -25,41 +25,52 @@ function UnauthenticatedMain(props){
         },
         {
             title: "test 1",
-            start: new Date("March 3, 2022"),
-            end: new Date("March 3, 2022")
+            start: new Date("03/03/2022"),
+            end: new Date("03/03/2022")
         },
         {
             title: "test 2",
-            start: new Date("March 3, 2022"),
-            end: new Date("March 3, 2022")
+            start: new Date("03/03/2022"),
+            end: new Date("03/03/2022")
         },
         {
             title: "test 3",
-            start: new Date("March 3, 2022"),
-            end: new Date("March 3, 2022")
+            start: new Date("03/03/2022"),
+            end: new Date("03/03/2022")
         },
         {
             title: "test 4",
-            start: new Date("March 3, 2022"),
-            end: new Date("March 3, 2022")
+            start: new Date("03/03/2022"),
+            end: new Date("03/03/2022")
         },
         {
             title: "test 5",
-            start: new Date("March 3, 2022"),
-            end: new Date("March 3, 2022")
+            start: new Date("03/03/2022"),
+            end: new Date("03/03/2022")
         },
         {
             title: "test 6",
-            start: new Date("March 3, 2022"),
-            end: new Date("March 3, 2022")
+            start: new Date("03/04/2022"),
+            end: new Date("03/07/2022")
         }
     ];
+
+    function changeDate(info){
+        for (let i=0; i<myEventsList.length; i++){
+            if(myEventsList[i].title === info.title){
+                console.log(newDate)
+                myEventsList[i].start = newDate;
+                myEventsList[i].end = newDate;
+            }
+        }
+    }
 
     return(
         <div>
             <div className="UnauthMain">
                 <h1>Can have news or recent company score here.</h1>
                 <h1>Top Drivers.</h1>
+                <input type="date" onChange={e => setNewDate(moment(e.target.value).format("L"))}/>
                 <Calendar
                     localizer={localizer}
                     events={myEventsList}
@@ -67,7 +78,7 @@ function UnauthenticatedMain(props){
                     endAccessor="end"
                     defaultView="week"
                     resizable
-                    onSelectEvent={event => console.log(event)}
+                    onSelectEvent={event => changeDate(event)}
                 />
                 <h1>Can have news or recent company score here.</h1>
                 <h1>Top Drivers.</h1>

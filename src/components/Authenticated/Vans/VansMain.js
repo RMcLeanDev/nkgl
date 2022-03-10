e90dfa1fd8c75f8d8dbae413785357795b76179b
rmcleandev <rmcleandev@gmail.com>
2022-03-10 12:47:02 -0800

    attempting to sort calendar items based on title.

--- src/components/Authenticated/Vans/VansMain.js
+++ src/components/Authenticated/Vans/VansMain.js
@@ -14,6 +14,7 @@ import AddVanMaintenance from './AddVanMaintenance';
 function VansMain(props){
 
     let adjustDatesComponent;
+    let newArray = [];
     const myEventsList = [];
     let localizer = momentLocalizer(moment)
     const [adjustDateComponent, setAdjustDateComponent] = useState({"state": false, info :{}})
